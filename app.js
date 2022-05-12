c86ddf46fc54fa56561c9a2bdcf29495846ecdd2
Rtmorais <rt.morais@hotmail.com>
2022-05-12 00:34:05 -0300

    Expanded Menu - DONE

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ function onScroll() {
     navigation.classList.add('scroll')
   } else navigation.classList.remove('scroll')
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
