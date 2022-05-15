683f17c64bcd5a57188f4ffd71f602711addf85d
Rtmorais <rt.morais@hotmail.com>
2022-05-15 13:04:16 -0300

    ScrollReveal Added

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home,
+#home img,
+#home .numDiv,
+#services,
+#services header,
+#services .card
+`)
